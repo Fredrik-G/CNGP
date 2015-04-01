0a0c60ab51e927963b6e8b05ccc821c934f0d357
Fredrik Gummus <gamwed@gmail.com>
2015-04-01 11:39:15 +0200

    Klasskelett för User, IpInfo, Statistics, ActiveSkill, BuffEffect, Effect, Team, Game, TurnbasedSpells.

---
+++ Assets/Engine/BuffEffect.cs
@@ -0,0 +1,22 @@
+﻿namespace Engine
+{
+    /// <summary>
+    /// Class that contains information about one buff/effect.
+    /// </summary>
+    class BuffEffect
+    {
+        public int Duration { get; set; }
+        public string Info { get; set; }
+        public int Cooldown { get; set; }
+
+        public Effect Effect { get; set; }
+
+        public BuffEffect(int duration, string info, int cooldown, Effect effect)
+        {
+            Duration = duration;
+            Info = info;
+            Cooldown = cooldown;
+            Effect = effect;
+        }
+    }
+}
---
+++ Assets/Engine/Game.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Engine
+{
+    /// <summary>
+    /// Class that holds information about a whole game.
+    /// </summary>
+    class Game
+    {
+        public Map map { get; set; }
+        private List<Team> _teams = new List<Team>();
+
+        //TODO: fixa result
+        public int Result { get; set; }
+    }
+}
